9b314aef980a5d8b3cd11b8cb1caf554d4addcc5
DanielBerns <daniel.w.berns@gmail.com>
2021-05-31 16:35:56 -0300

    alpha example from flask quickstart

---
+++ code/alpha/firstapp.py
@@ -0,0 +1,11 @@
+"""
+See https://flask.palletsprojects.com/en/2.0.x/quickstart/
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
